93a62146bae880816c27df892e0643a6af14be7d
Tithira Mojitha Ranasinghe <127576267+MojithaR@users.noreply.github.com>
2024-08-19 16:16:39 +0530

    Eyes of mine initial commits

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import AdminView from './components/AdminView';
+
+function App() {
+  return (
+    <div className="App">
+      <AdminView />
+    </div>
+  );
+}
+
+export default App;
+//cd D:\NodeJS\React_JS\Pracaa\practice01
+//yarn install
+//yarn start
